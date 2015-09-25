6a8a418ab08e2e24d10145d5dfaa82a8d47bc70a
citizenkane <kagarwa@ncsu.edu>
2015-09-25 16:06:27 -0400

    drop and create users

--- db/migrate/20150924034055_create_users.rb
+++ db/migrate/20150924034055_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table :users do |t|
 	t.column :email, :string, :limit => 100, :null => false
 	t.column :password, :string, :limit => 100, :null => false
@@ -10,7 +11,7 @@ class CreateUsers < ActiveRecord::Migration
     end
      User.create :email => "abc@ncsu.edu", :password => "ncsu", :name => "Ncsu", :is_admin => "Y"
      User.create :email => "kagarwa@ncsu.edu", :password => "kagarwa", :name => "Krishna Agarwala", :is_admin => "N"    
-     User.create :email => "admin@lib.com", :password => "admin", :name => "Preconfigured Admin", :is_admin => true
+     User.create :email => "admin@lib.com", :password => "admin", :name => "Preconfigured Admin", :is_admin => TRUE
 
   end
 end
